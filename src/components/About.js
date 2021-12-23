4c7ae7362f54a5a5cb40179d29c40dd0d2fc85e0
Daniel Williams <danielpaulwill@gmail.com>
2021-12-22 21:35:29 -0500

    commit tree 0a5262a41714a8f063eecefe72dad7e16be19359

--- src/components/About.js
+++ src/components/About.js
@@ -5,7 +5,7 @@ function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      {!null | "" ? <p>{ props.bio }</p> : null}
+      {props.bio ? <p>{ props.bio }</p> : null}
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
       <Links 
       github={props.github}
